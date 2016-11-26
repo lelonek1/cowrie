9c14edbefde0a19323083c22ba0353a786e6bfc5
Ben Lelonek <lelonek1@umbc.edu>
2016-11-26 13:36:09 -0500

    Basic profile loading support functional
    Profiles can currently override any setting specified in cowrie.cfg

--- cowrie/core/config.py
+++ cowrie/core/config.py
@@ -6,9 +6,27 @@ This module contains ...
 """
 
 import configparser
+import shlex
+import os
 
 def readConfigFile(cfgfile):
     config = configparser.ConfigParser()
     config.read(cfgfile)
     return config
 
+def loadProfiles(cfg):
+    if cfg.has_option('profile', 'profile_directory'):
+        profile_directory = cfg.get('profile', 'profile_directory')
+    else:
+        profile_directory = 'etc/profiles'
+
+    if cfg.has_option('profile', 'enabled_profiles'):
+        enabled_profiles = cfg.get('profile', 'enabled_profiles').split('\n')
+        for profile_path in enabled_profiles:
+            try:
+                cfg.read(os.path.join(profile_directory, profile_path, 'profile.cfg'))
+            except configparser.Error as e:
+                print('WARNING: Profile %s could not be loaded due to error: %r'
+                      % (profile_path, e))
+
+    return cfg
